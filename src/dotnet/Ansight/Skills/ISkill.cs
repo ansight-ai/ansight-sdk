fad86d2fac2b58a127be86190af587302d7c8ffe
Matthew Robbins <matthew.ch.robbins@gmail.com>
2026-03-13 09:13:34 +1100

    Start defining Skills contracts

--- src/dotnet/Ansight/Skills/ISkill.cs
+++ src/dotnet/Ansight/Skills/ISkill.cs
@@ -2,9 +2,15 @@ namespace Ansight.Skills;
 
 public interface ISkill
 {
+    string Category { get; }
+    
+    string Id { get; }
+    
     string Name { get; }
     
     string Description { get; }
     
-    
+    string Keywords { get; }
+
+    Task<SkillResult> Execute(IReadOnlyDictionary<string, string> arguments);
 }
---
+++ src/dotnet/Ansight/Skills/SkillResult.cs
@@ -0,0 +1,6 @@
+namespace Ansight.Skills;
+
+public class SkillResult
+{
+    // TODO: Define how a skill result should be returned for use with an agent.
+}
