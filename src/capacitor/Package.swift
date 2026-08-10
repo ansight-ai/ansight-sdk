cbe283b9a8fdd520300dd5972fd5ed424fa17f28
Matthew Robbins <matthew.ch.robbins@gmail.com>
2026-08-10 10:24:27 +1000

    chore(release): prepare SDK 1.3.0-preview.1

--- src/capacitor/Package.swift
+++ src/capacitor/Package.swift
@@ -7,7 +7,7 @@ let ansightSdkDependency: Package.Dependency = useLocalAnsightSdk
     ? .package(name: "AnsightSDK", path: "../ios")
     : .package(
         url: "https://github.com/ansight-ai/ansight-sdk.git",
-        exact: "1.2.0-preview.3"
+        exact: "1.3.0-preview.1"
     )
 let ansightTargetDependency: Target.Dependency = .product(
     name: "Ansight",
--- src/ios/AnsightCore.podspec
+++ src/ios/AnsightCore.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name         = "AnsightCore"
-  s.version      = "1.2.0-preview.3"
+  s.version      = "1.3.0-preview.1"
   s.summary      = "Native iOS runtime for Ansight"
   s.homepage     = "https://github.com/ansight-ai/ansight-sdk"
   s.license      = { :type => "Ansight SDK Source-Available License", :file => "LICENSE" }
